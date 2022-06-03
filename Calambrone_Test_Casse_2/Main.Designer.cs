e9ace360e9fbc533f87d971f8d0682fc26fdb6c1
Walter Setti <walter.setti@iit.it>
2022-06-03 13:49:45 +0200

    Main form

--- Calambrone_Test_Casse_2/Main.Designer.cs
+++ Calambrone_Test_Casse_2/Main.Designer.cs
@@ -51,6 +51,7 @@
             this.Name = "Main";
             this.Text = "Test_Casse";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Calambrone_Test_Casse_2/Main.cs
+++ Calambrone_Test_Casse_2/Main.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using Newtonsoft.Json;
+using System.Threading;
+
+
 namespace Calambrone_Test_Casse_2
 {
+    public delegate void ResumeFromMessage();
     public partial class Main : Form
     {
+        public static readonly string resourcesPath = Path.GetDirectoryName(Application.ExecutablePath) + "\\resources";
+
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
